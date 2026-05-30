1aeb4a198e3ca02d9e594d5689b620a02b88c49c
Saniya-712006 <sksaniya2019@gmail.com>
2026-05-30 11:58:55 +0530

    Add physician selection functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+// page.tsx
 "use client";
 
 import { useEffect, useState } from "react";
@@ -7,7 +8,8 @@ import PhysicianCard from "@/components/PhysicianCard";
 
 export default function Home() {
   const [physicians, setPhysicians] = useState<Physician[]>([]);
-
+  const [selectedPhysicians, setSelectedPhysicians] =
+  useState<string[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getPhysicians();
@@ -17,17 +19,35 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const togglePhysician = (id: string) => {
+  setSelectedPhysicians((prev) =>
+    prev.includes(id)
+      ? prev.filter((p) => p !== id)
+      : [...prev, id]
+  );
+};
+
   return (
     <main className="p-8">
       <h1 className="text-3xl font-bold mb-6">
         Physician Discovery
       </h1>
+      <div className="mb-4 font-medium">
+          {physicians.length} Physicians |
+          {" "}
+          {selectedPhysicians.length} Selected
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+        
         {physicians.map((physician) => (
           <PhysicianCard
             key={physician.id}
             physician={physician}
+            isSelected={selectedPhysicians.includes(
+              physician.id
+            )}
+            onToggle={togglePhysician}
           />
         ))}
       </div>
--- frontend/components/PhysicianCard.tsx
+++ frontend/components/PhysicianCard.tsx
@@ -1,15 +1,27 @@
+// PhysicianCard.tsx
 import { Physician } from "@/types/physician";
 
 interface Props {
   physician: Physician;
+  isSelected: boolean;
+  onToggle: (id: string) => void;
 }
 
-export default function PhysicianCard({ physician }: Props) {
+export default function PhysicianCard({ physician, isSelected, onToggle }: Props) {
   return (
     <div className="border rounded-lg p-4 shadow-sm">
+        
       <h2 className="text-lg font-semibold">
-        Dr. {physician.firstName} {physician.lastName}
+        Dr. {physician.firstName} {physician.lastName} 
+         {" "}
+        <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={() => onToggle(physician.id)}
+        />
+        
       </h2>
+      
 
       <p>{physician.specialty}</p>
 
@@ -20,6 +32,8 @@ export default function PhysicianCard({ physician }: Props) {
       <p className="text-sm">
         {physician.city}, {physician.state}
       </p>
+
+      
     </div>
   );
 }
